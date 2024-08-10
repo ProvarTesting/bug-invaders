3ef4b1fd8dad91923205eeef98e0900e47d8d878
Ajay Kumar <92162678+ajaylamba-provar@users.noreply.github.com>
2024-08-11 01:08:20 +0530

    Added functionality to send contact details to Google Sheets

--- js/Utils.js
+++ js/Utils.js
@@ -11,7 +11,23 @@ invadersApp.utils.addText = function (game, x, y, text, size) {
     var img = game.add.image(x, y, font);
     img.scale.setTo(scale, scale);
     img.anchor.setTo(0.5, 0.5);
-    return {'font': font, 'img': img};
+    return { 'font': font, 'img': img };
+};
+
+invadersApp.utils.makeTextBlink = function (game, img, blinkInterval) {
+    if (blinkInterval) {
+        img.blinkTimer = game.time.events.loop(blinkInterval, function () {
+            img.visible = !img.visible;
+        });
+    }
+};
+
+invadersApp.utils.stopTextBlink = function (game, img) {
+    if (img.blinkTimer) {
+        game.time.events.remove(img.blinkTimer);
+        img.blinkTimer = null;
+        img.visible = true; // Ensure the text is visible when blinking stops
+    }
 };
 
 invadersApp.utils.pad = function (n, width, z) {
@@ -19,3 +35,34 @@ invadersApp.utils.pad = function (n, width, z) {
     n = n + '';
     return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
 };
+
+// Function to show alert using Minecraft font
+invadersApp.utils.showMinecraftAlert = function (game, message) {
+    var style = { font: "32px Minecraft", fill: "#ff0044", align: "center" };
+    var alertText = game.add.text(game.world.centerX, game.world.centerY, message, style);
+    alertText.anchor.set(0.5);
+
+    // Optional: Add a background for the alert
+    var alertBackground = game.add.graphics();
+    alertBackground.beginFill(0x000000, 0.8);
+    alertBackground.drawRect(alertText.x - alertText.width / 2 - 10, alertText.y - alertText.height / 2 - 10, alertText.width + 20, alertText.height + 20);
+    alertBackground.endFill();
+    alertBackground.anchor.set(0.5);
+
+    // Bring text to the front
+    game.world.bringToTop(alertText);
+
+    // Hide alert after a few seconds
+    game.time.events.add(Phaser.Timer.SECOND * 2, function () {
+        alertText.destroy();
+        alertBackground.destroy();
+    }, this);
+}
+
+// Function to create email text using Minecraft font
+invadersApp.utils.createInputText = function (game, x, y, message) {
+    var style = { font: "32px Minecraft", fill: "#ffffff", align: "center" };
+    var inputText = game.add.text(x, y, message, style);
+    inputText.anchor.set(0.5);
+    return inputText;
+}
